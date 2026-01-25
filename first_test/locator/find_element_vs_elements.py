7f17a3721ea3d2b6f612ca57cd80619716557404
openthedidi <openthedidi2004@gmail.com>
2026-01-25 17:49:38 +0800

    [new]:find_element vs find_elements 差異

---
+++ first_test/locator/find_element_vs_elements.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+chrom_drive = webdriver.Chrome()
+
+chrom_drive.get("https://demo.nopcommerce.com")
+chrom_drive.maximize_window()
+
+# ============find_element=============
+
+# find_element return only one element，如果沒有就會噴NoSuchElementException
+input_search = chrom_drive.find_element(
+    By.XPATH, "//input[@id='small-searchterms']")
+print(input_search.is_enabled())
+input_search.send_keys("20")
+
+time.sleep(5)
+
+
+# find_element return multi elements，如果沒有就會噴NoSuchElementException
+# 只會回傳第一個找到的element
+
+a_href = chrom_drive.find_element(
+    By.XPATH, "//div[@class='footer-menu']//a")
+
+print(a_href.text)
+
+
+# =============find_elements=============
+a_hrefs = chrom_drive.find_elements(
+    By.XPATH, "//div[@class='footer-menu']//a")
+print(type(a_hrefs))
+print(len(a_hrefs))
+
+# 就算找不到也不會噴NoSuchElementException
+empty_hrefs = chrom_drive.find_elements(
+    By.XPATH, "//div[@class='footer-menu']//tr")
+print(len(empty_hrefs))
