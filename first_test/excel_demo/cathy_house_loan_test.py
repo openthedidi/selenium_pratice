32970b5026aeedfac2cb5da3891c794ba9ef514a
openthedidi <openthedidi2004@gmail.com>
2026-01-28 20:10:33 +0800

    [update]:更新國X房貸試算檢測腳本

--- first_test/excel_demo/cathy_house_loan_test.py
+++ first_test/excel_demo/cathy_house_loan_test.py
@@ -28,9 +28,9 @@ excel_utils.getRowCount(excel_file, "Sheet2")
 title_data = excel_utils.readRow(excel_file, "Sheet2", 1)
 
 row1_data = excel_utils.readRow(excel_file, "Sheet2", 2)
-print(row1_data)
+
 row2_data = excel_utils.readRow(excel_file, "Sheet2", 3)
-print(row2_data)
+
 
 # 用dict , title is key and row-data is value
 
@@ -42,60 +42,9 @@ row2_data_dict = {}
 for i in range(len(title_data)):
     row2_data_dict[title_data[i]] = row2_data[i]
 
-print(row1_data_dict)
-print(row2_data_dict)
-
 total_test_data = []
 total_test_data.append(row1_data_dict)
 total_test_data.append(row2_data_dict)
-
-
-# ---- get html element
-
-
-# loan_amount_input = wait_setting.until(
-#     EC.presence_of_element_located((By.ID, "amount")))
-# loan_amount_input.send_keys(1000)
-
-
-# loan_year_radio_label = wait_setting.until(
-#     EC.presence_of_element_located((By.XPATH, "//label[@for='tab2']")))
-# chrom_drive.execute_script("arguments[0].click();", loan_year_radio_label)
-# fee = wait_setting.until(
-#     EC.presence_of_element_located((By.NAME, "Fee")))
-# fee.send_keys(255)
-
-# extensionY = wait_setting.until(
-#     EC.presence_of_element_located((By.XPATH, "//input[@id='extensionY']")))
-# extensionY.send_keys(10)
-
-# pay_way_radio_label = wait_setting.until(
-#     EC.presence_of_element_located((By.XPATH, "//label[@for='customPayFor1']")))
-# chrom_drive.execute_script("arguments[0].click();", pay_way_radio_label)
-
-# rat_label = wait_setting.until(
-#     EC.presence_of_element_located((By.XPATH, "//label[@for='tab-list-1']")))
-# chrom_drive.execute_script("arguments[0].click();", rat_label)
-
-# period1_input = wait_setting.until(
-#     EC.presence_of_element_located((By.ID, "period1")))
-# period1_input.clear()
-# period1_input.send_keys(1)
-
-# period2_input = wait_setting.until(
-#     EC.presence_of_element_located((By.ID, "period11")))
-# period2_input.clear()
-# period2_input.send_keys(240)
-
-# periodrate1_input = wait_setting.until(
-#     EC.presence_of_element_located((By.ID, "periodrate1")))
-# periodrate1_input.clear()
-# periodrate1_input.send_keys(2)
-
-# start_btn = wait_setting.until(
-#     EC.presence_of_element_located((By.ID, "formSubmitBtn")))
-
-# chrom_drive.execute_script("arguments[0].click();", start_btn)
 
 row = 2
 
@@ -155,23 +104,29 @@ for test_datas in total_test_data:
     result_span_list = wait_setting.until(
         EC.presence_of_all_elements_located((By.XPATH, "//span[@class='cubinvest-highlight']")))
 
-    if (result_span_list[0].text != test_datas['第1 ~ 36個月預期']):
-        print(f"第1 ~ 36個月預期: {test_datas['第1 ~ 36個月預期']}")
-        print(f"第1 ~ 36個月實際: {result_span_list[0].text}")
-        excel_utils.writeData(excel_file, "Sheet2", row, 13,
-                              f"第1 ~ 36個月實際: {result_span_list[0].text}")
-        excel_utils.fillRedColor(excel_file, "Sheet2", row, 13)
-    elif (result_span_list[1].text != test_datas['第37 ~ 239個月預期']):
-        print(f"第37 ~ 239個月預期: {test_datas['第37 ~ 239個月預期']}")
-        print(f"第37 ~ 239個月實際: {result_span_list[1].text}")
-        excel_utils.writeData(excel_file, "Sheet2", row, 13,
-                              f"第37 ~ 239個月預期: {result_span_list[0].text}")
-        excel_utils.fillRedColor(excel_file, "Sheet2", row, 13)
-    elif (result_span_list[2].text != test_datas['第240個月預期']):
-        print(f"第240個月預期: {test_datas['第240個月預期']}")
-        print(f"第240個月實際: {result_span_list[2].text}")
-        excel_utils.writeData(excel_file, "Sheet2", row, 13,
-                              f"第240個月預期: {result_span_list[0].text}")
+    expected_1 = test_datas['第1 ~ 36個月預期']
+    expected_2 = test_datas['第37 ~ 239個月預期']
+    expected_3 = test_datas['第240個月預期']
+
+    actual_1 = result_span_list[0].text
+    actual_2 = result_span_list[1].text
+    actual_3 = result_span_list[2].text
+
+    print(f"第1~36 預期:{expected_1} 實際:{actual_1}")
+    print(f"第37~239 預期:{expected_2} 實際:{actual_2}")
+    print(f"第240 預期:{expected_3} 實際:{actual_3}")
+
+    errors = []
+    if actual_1 != expected_1:
+        errors.append(f"第1~36 預期:{expected_1} 實際:{actual_1}")
+    if actual_2 != expected_2:
+        errors.append(f"第37~239 預期:{expected_2} 實際:{actual_2}")
+    if actual_3 != expected_3:
+        errors.append(f"第240 預期:{expected_3} 實際:{actual_3}")
+
+    if errors:
+        excel_utils.writeData(excel_file, "Sheet2", row,
+                              13, " ; ".join(errors))
         excel_utils.fillRedColor(excel_file, "Sheet2", row, 13)
     else:
         excel_utils.writeData(excel_file, "Sheet2", row, 13, "OK")
@@ -180,6 +135,4 @@ for test_datas in total_test_data:
 
 
 workbook.close()
-
-input("Press Enter to close the browser...")
 chrom_drive.quit()
