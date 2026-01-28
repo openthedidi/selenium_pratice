39700f3f13b8d8d4063f29a77ae4560eb49f9f68
openthedidi <openthedidi2004@gmail.com>
2026-01-28 09:46:04 +0800

    [new]:特殊selection操作

---
+++ first_test/element_operate/selection_without_optoin.py
@@ -0,0 +1,40 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+chrom_drive = webdriver.Chrome()
+chrom_drive.get(
+    "https://www.dummyticket.com/dummy-ticket-for-visa-application/")
+chrom_drive.maximize_window()
+
+wait_setting = WebDriverWait(
+    chrom_drive, timeout=5, ignored_exceptions=Exception)
+
+
+chrom_drive.execute_script("window.scrollTo(0, 200);")
+
+
+span_btn = wait_setting.until(
+    EC.presence_of_element_located((By.XPATH, "//span[@id='select2-billing_country-container']")))
+span_btn.click()
+
+time.sleep(3)
+
+option_btn_list = wait_setting.until(
+    EC.presence_of_all_elements_located((By.XPATH, "//ul[@class='select2-results__options']//li[@class='select2-results__option']")))
+
+for option_btn in option_btn_list:
+    if (option_btn.text == "Yemen"):
+        option_btn.click()
+        break
+
+
+input("Press Enter to close the browser...")
+chrom_drive.quit()
+
+
+#
